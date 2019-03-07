12e49c4d6e5963dd5d0ab5a96ec9b9efc56e6f10
ESTRADA Nicolas <nicolas.estrada@oscaro.com>
2019-03-07 19:51:03 +0100

    Added repositories

--- core/project.clj
+++ core/project.clj
@@ -1,6 +1,9 @@
 (defproject com.oscaro/macrometer.core "1.1.3.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
                  [io.micrometer/micrometer-core "1.1.3"]]
+  :repositories [["oscaro" {:url "https://artifactory.oscaroad.com/artifactory/libs-release-local"}]
+                 ["oscaro-ext" {:url "https://artifactory.oscaroad.com/artifactory/ext-release-local"}]
+                 ["oscaro-snapshot" {:url "https://artifactory.oscaroad.com/artifactory/libs-snapshot-local"}]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]]
                    :source-paths ["dev"]}}
   :repl-options {:init-ns user}
--- prometheus/project.clj
+++ prometheus/project.clj
@@ -4,6 +4,9 @@
                  [com.oscaro/macrometer.core "1.1.3.0-SNAPSHOT"]
                  [io.micrometer/micrometer-registry-prometheus "1.1.3"]
                  [integrant "0.7.0"]]
+  :repositories [["oscaro" {:url "https://artifactory.oscaroad.com/artifactory/libs-release-local"}]
+                 ["oscaro-ext" {:url "https://artifactory.oscaroad.com/artifactory/ext-release-local"}]
+                 ["oscaro-snapshot" {:url "https://artifactory.oscaroad.com/artifactory/libs-snapshot-local"}]]
   :profiles {:dev {:dependencies [[io.pedestal/pedestal.service-tools "0.5.5"]
                                   [io.pedestal/pedestal.jetty "0.5.5"]]
                    :source-paths ["dev"]}}
