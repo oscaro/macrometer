81a65b26114566943cad6405d61c5e0e558b913e
ESTRADA Nicolas <nicolas.estrada@oscaro.com>
2019-07-23 17:46:51 +0200

    Version bump

--- prometheus/project.clj
+++ prometheus/project.clj
@@ -1,17 +1,16 @@
-(defproject com.oscaro/macrometer.prometheus "1.1.4.1"
+(defproject com.oscaro/macrometer.prometheus "1.2.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
-                 [org.clojure/tools.logging "0.4.1"]
-                 [com.oscaro/macrometer.core "1.1.4.0"]
-                 [io.micrometer/micrometer-registry-prometheus "1.1.4"]
+                 [org.clojure/tools.logging "0.5.0"]
+                 [com.oscaro/macrometer.core "1.2.0.0-SNAPSHOT"]
+                 [io.micrometer/micrometer-registry-prometheus "1.2.0"]
                  [integrant "0.7.0"]
                  [io.prometheus/simpleclient_common "0.6.0"]
-                 [io.pedestal/pedestal.service "0.5.5" :scope "provided"]]
+                 [io.pedestal/pedestal.service "0.5.7" :scope "provided"]]
   :repositories [["oscaro" {:url "https://artifactory.oscaroad.com/artifactory/libs-release-local"}]
                  ["oscaro-ext" {:url "https://artifactory.oscaroad.com/artifactory/ext-release-local"}]
                  ["oscaro-snapshot" {:url "https://artifactory.oscaroad.com/artifactory/libs-snapshot-local"}]]
-  :profiles {:dev {:dependencies [[io.pedestal/pedestal.service-tools "0.5.5"]
-                                  [io.pedestal/pedestal.jetty "0.5.5"]
-                                  [io.prometheus/simpleclient_hotspot "0.6.0"]]
+  :profiles {:dev {:dependencies [[io.pedestal/pedestal.service-tools "0.5.7"]
+                                  [io.pedestal/pedestal.jetty "0.5.7"]]
                    :source-paths ["dev"]}}
   :repl-options {:init-ns user}
   :global-vars {*warn-on-reflection* true})
