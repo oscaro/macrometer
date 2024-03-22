8d9f3a1bbd2fa598b78e0e36a0d6b70a01ee634d
iomonad <iomonad@riseup.net>
2024-03-22 10:39:28 +0100

    pre-release: 1.12.4.0-SNAPSHOT

--- core/project.clj
+++ core/project.clj
@@ -1,7 +1,7 @@
-(defproject com.oscaro/macrometer.core "1.11.0.1-SNAPSHOT"
+(defproject com.oscaro/macrometer.core "1.12.4.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure           "1.10.1" :scope "provided"]
-                 [org.clojure/core.async        "1.6.673"]
-                 [io.micrometer/micrometer-core "1.11.0"]]
+                 [org.clojure/core.async        "1.6.681"]
+                 [io.micrometer/micrometer-core "1.12.4"]]
   :deploy-repositories [["snapshots" {:url "https://repo.clojars.org"
                                       :username :env/clojars_username
                                       :password :env/clojars_password
@@ -10,8 +10,8 @@
                                       :username :env/clojars_username
                                       :password :env/clojars_password
                                       :sign-releases false}]]
-  :profiles {:dev {:dependencies [[org.clojure/tools.namespace    "1.4.4"]
-                                  [org.eclipse.jetty/jetty-server "11.0.15"]
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace    "1.5.0"]
+                                  [org.eclipse.jetty/jetty-server "12.0.7"]
                                   [clj-http                       "3.12.3"]]
                    :source-paths ["dev"]}}
   :repl-options {:init-ns user}
